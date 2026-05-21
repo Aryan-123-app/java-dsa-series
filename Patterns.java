1596b031cd56d59fbc6b356ca004d966216ed9aa
Aryan-123-app <aryanpatade8@gmail.com>
2026-05-21 21:29:36 +0530

    Pattern 1 - Filled Square

--- Patterns.java
+++ Patterns.java
@@ -1,30 +1,5 @@
-import java.util.Scanner;
-public class Patterns {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter value of rows: ");
-        int rows = sc.nextInt();
-
-        System.out.print("Enter value of columns: ");
-        int cols = sc.nextInt();
-
-        for(int i = 1; i <= rows; i++){
-            for(int j = 1; j <= cols; j++){
-                if(i == 1 || i == rows ){
-                    System.out.print("* ");
-                }
-                else {
-                    if(j == 1 || j == cols){
-                        System.out.print("* ");
-                    }
-                    else{
-                        System.out.print("  ");
-                    }
-                }
-            }
-            System.out.println();
-        }
-
+public class Patterns{
+    public static void main(String[] args){
+        System.out.println("Hello I'm Java!");
     }
 }
