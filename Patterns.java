ecfd01c2b4c48c06fdfbed2a1594d747802ea10e
Aryan-123-app <aryanpatade8@gmail.com>
2026-05-21 21:36:48 +0530

    Pattern 4 - Hollow Rectangle

--- Patterns.java
+++ Patterns.java
@@ -5,6 +5,6 @@ public class Patterns{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first value : ");
         int a = sc.nextInt();
-        a = a + 10;
-        System.out.print("a + 10 = " + a);
+        int b = a * 10;
+        System.out.print("b = " + b);
     }}
