87937cfe5ac39e5a006a6b81e92c253c38d40465
Aryan-123-app <aryanpatade8@gmail.com>
2026-05-21 21:35:23 +0530

    Pattern 3 - Hollow Square

--- Patterns.java
+++ Patterns.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Patterns{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = sc.next();
-        System.out.print("Name: "+ name);
+        System.out.print("Enter first value : ");
+        int a = sc.nextInt();
+        a = a + 10;
+        System.out.print("a + 10 = " + a);
     }}
